c0a93de102d05a66b4454487cd5e40fdd868960c
jessica <jessica@everybill.com>
2018-05-10 11:08:07 -0400

    Updated files. TODO: Connect them.

---
+++ test1.js
@@ -0,0 +1,26 @@
+/*var testApp = angular.module('paymentApp', ['ngSanitize', 'ngMessages']);
+
+testApp.directive('test1', function () {
+	return {
+		templateUrl: '/folder/path/file.htm',
+		controller: 'test1'
+	};
+});
+
+testApp.controller('test1', function ($scope, $filter, $http, $location, $rootScope, $anchorScroll, $window, payForm, $q, $timeout) {
+
+	$scope.spanishSiteDomains = [
+		'acmepago.com',
+		'pagamoorelaw.com',
+		'pagatitanreceivables.com',
+		'pagaeverybillhealthcare.com'
+	];
+	$scope.foo = 'loading';
+	$scope.email = {};
+	$rootScope.pageTitle = '';
+	$scope.errors = {};
+
+/*});*/
+
+
+document.getElementById("testContent").innerHTML = "Hello Helix ALM!";
